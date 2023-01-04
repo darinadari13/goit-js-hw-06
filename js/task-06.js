c243afe047545733abe54fbe0085f7db90133f0a
darinadari13 <darina460@gmail.com>
2023-01-04 10:23:38 +0200

    chore

--- js/task-06.js
+++ js/task-06.js
@@ -2,6 +2,7 @@ const inputValue = document.querySelector('#validation-input');
 
 inputValue.addEventListener('blur', e => {
   e.preventDefault();
+  inputValue.classList.remove('valid', 'invalid');
 
   if (e.target.value.length === 6) {
     inputValue.classList.toggle('valid')
